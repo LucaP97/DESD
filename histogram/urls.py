adf5cd9cfabfe4701a7de203783e141cc9200dda
Luca <luca.pike7@gmail.com>
2022-10-17 11:47:28 +0100

    2nd week, part 1 complete

--- histogram/urls.py
+++ histogram/urls.py
@@ -3,4 +3,7 @@ from histogram import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("histogram/<name>", views.hello_there),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 ]
--- histogram/views.py
+++ histogram/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from histogram import createHistogram
+import datetime
 
+
+# def home(request):
+#     #return HttpResponse("Hello, Django")
+#     return HttpResponse(createHistogram.runHistogram())
 
 def home(request):
-    #return HttpResponse("Hello, Django")
-    return HttpResponse(createHistogram.runHistogram())
+    return render(request, "histogram/home.html")
+
+def about(request):
+    return render(request, "histogram/about.html")
+
+def contact(request):
+    return render(request, "histogram/contact.html")
 
 # Create your views here.
+
+def hello_there(request, name):
+    return render(
+        request,
+        'histogram/histogram_there.html',
+        {
+            'name': name,
+            'date': datetime.datetime.now()
+        }
+    )
+
+
+
+# def hello_there(request):
+#     context = {'Luca': 'date'}
+#     return render(
+#         request,
+#         'histogram/histogram_there.html',
+#         context
+#     )
